a24f924b308e67899d40225ddc35472de375b3ea
rud-wtag <127865276+rud-wtag@users.noreply.github.com>
2023-04-13 01:06:22 +0600

    worked on step 4

--- src/components/ui/TaskFooter.jsx
+++ src/components/ui/TaskFooter.jsx
@@ -4,9 +4,9 @@ import { ReactComponent as DoneIcon } from 'assets/ok.svg';
 import { ReactComponent as EditIcon } from 'assets/edit.svg';
 import { ReactComponent as DeleteIcon } from 'assets/delete.svg';
 import Button from 'components/ui/Button';
-import { deleteTodo } from 'redux/actions/TodoAction';
+import { deleteTodo, setTOComplete } from 'redux/actions/TodoAction';
 import { useDispatch } from 'react-redux';
-export default function TaskFooter({ isCompleted = false, taskId }) {
+export default function TaskFooter({ isCompleted = null, taskId }) {
   const dispatch = useDispatch();
 
   const deleteHandler = (e) => {
@@ -14,12 +14,17 @@ export default function TaskFooter({ isCompleted = false, taskId }) {
     dispatch(deleteTodo(taskId));
   };
 
+  const completeHandler = (e) => {
+    e.preventDefault();
+    dispatch(setTOComplete(taskId));
+  };
+
   return (
     <div className="task__footer">
       <div className="task__footer__left">
         {!isCompleted && (
           <>
-            <Button>
+            <Button onClick={completeHandler}>
               <DoneIcon />
             </Button>
             <Button>
--- src/redux/reducers/TodoReducer.js
+++ src/redux/reducers/TodoReducer.js
@@ -1,5 +1,5 @@
 import { actionTypes } from 'redux/constants/ActionTypes';
-const { ADD_TODO, SET_ADD_TASK, DELETE_TODO } = actionTypes;
+const { ADD_TODO, SET_ADD_TASK, DELETE_TODO, COMPLETE_TASK } = actionTypes;
 const initialState = {
   isAddingTask: false,
   todos: []
@@ -24,6 +24,17 @@ export const todoReducer = (state = initialState, action) => {
         ...state,
         todos: newList
       };
+    case COMPLETE_TASK:
+      newList = state.todos.map((todo) => {
+        if (todo.id === action.payload.taskId) {
+          return {
+            ...todo,
+            completedAt: action.payload.completedAt
+          };
+        }
+        return todo;
+      });
+      return { ...state, todos: newList };
     default:
       return state;
   }
