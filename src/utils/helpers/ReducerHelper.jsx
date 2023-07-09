67a6d5382d29e91a19d87b025166e58fdf887d16
rud-wtag <127865276+rud-wtag@users.noreply.github.com>
2023-07-09 11:25:29 +0600

    merge: step-6 add tasks per page constant

--- src/utils/helpers/ReducerHelper.jsx
+++ src/utils/helpers/ReducerHelper.jsx
@@ -1,4 +1,6 @@
+import { filterActionTypes } from 'redux/constants/ActionTypes';
 import { TASKS_PER_PAGE } from 'utils/constants';
+const { ALL, COMPLETE, INCOMPLETE } = filterActionTypes;
 
 export const deleteTask = (todos, payload) => {
   return todos.filter((todo) => todo.id !== payload);
@@ -50,8 +52,29 @@ export const nextPage = (todos, currentPage) => {
   return 1;
 };
 
+const filterTasks = (tasks, filterState = ALL) => {
+  switch (filterState) {
+    case ALL:
+      return tasks;
+    case COMPLETE:
+      return tasks.filter((task) => {
+        if (task.completedAt !== null) return task;
+      });
+    case INCOMPLETE:
+      return tasks.filter((task) => {
+        if (task.completedAt === null) return task;
+      });
+    default:
+      return tasks;
+  }
+};
+
 export const paginate = (tasks, currentPage) => {
   const indexOfLastTask = currentPage * TASKS_PER_PAGE;
 
   return tasks.slice(0, indexOfLastTask);
+};
+
+export const searchAndFilter = (tasks, filter) => {
+  return filterTasks(tasks, filter.filterState);
 };
