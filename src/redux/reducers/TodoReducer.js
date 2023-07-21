c3bbea7ddb54a58c468f3993c4388546c31cde11
rud-wtag <127865276+rud-wtag@users.noreply.github.com>
2023-07-22 01:42:23 +0600

    merge: step-8 and style fix

--- src/redux/reducers/TodoReducer.js
+++ src/redux/reducers/TodoReducer.js
@@ -2,7 +2,7 @@ import { actionTypes } from 'redux/constants/ActionTypes';
 import { completeTask, deleteTask, editTask, setEditMode } from 'utils/helpers/ReducerHelper';
 const { ADD_TODO, SET_ADD_TASK, DELETE_TODO, COMPLETE_TASK, SET_EDIT, EDIT_TODO } = actionTypes;
 const initialState = {
-  isAddingTask: false,
+  isNewTaskRequested: false,
   todos: []
 };
 
@@ -20,7 +20,7 @@ export const todoReducer = (state = initialState, action) => {
     case SET_ADD_TASK:
       return {
         ...state,
-        isAddingTask: action.payload
+        isNewTaskRequested: action.payload
       };
     case DELETE_TODO:
       newList = deleteTask(state.todos, action.payload);
