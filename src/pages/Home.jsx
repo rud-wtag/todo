867fe6459e56f6d197487f7d1cb097e460b24460
rud-wtag <127865276+rud-wtag@users.noreply.github.com>
2023-07-26 19:17:52 +0600

    Feat: toast set and remove with timer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import BottomNav from 'components/BottomNav';
 import Heading from 'components/Heading';
 import TaskContainer from 'components/TaskContainer';
-import Toast from 'components/ui/Toast';
+import ToastContainer from '../components/ToastContainer';
 
 function Home() {
   return (
     <div className="home">
+      <ToastContainer />
       <div className="container">
         <Heading />
-        <Toast />
         <BottomNav />
         <TaskContainer />
       </div>
